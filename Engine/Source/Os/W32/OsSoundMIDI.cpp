b0e54feb1e61b15731a85efa913cf6b4ce35ad20
Pazaz <pazaz@itspazaz.com>
2026-07-23 11:33:32 -0400

    chore: Rely on external DirectMusic

--- Engine/Source/Os/W32/OsSoundMIDI.cpp
+++ Engine/Source/Os/W32/OsSoundMIDI.cpp
@@ -2,51 +2,15 @@
 
 #include "Services/AsyncFileRead.h"
 
+typedef unsigned long DWORD_PTR;
+
+#include <dmusici.h>
+
 struct ASYNCLOADER {
   CAsyncObject         *asyncLoader;
   TSGrowableArray<char> buffer;
 
   void Clear();
-};
-
-struct _DMUS_VERSION {
-  unsigned long dwVersionMS;
-  unsigned long dwVersionLS;
-};
-
-struct _DMUS_OBJECTDESC {
-  unsigned long dwSize;
-  unsigned long dwValidData;
-  GUID          guidObject;
-  GUID          guidClass;
-  FILETIME      ftDate;
-  _DMUS_VERSION vVersion;
-  wchar_t       wszName[64];
-  wchar_t       wszCategory[64];
-  wchar_t       wszFileName[260];
-  __int64       llMemLength;
-  unsigned int *pbMemData;
-  void         *pStream;
-};
-
-typedef _DMUS_OBJECTDESC DMUS_OBJECTDESC;
-
-struct IDirectMusicObject : public IUnknown {};
-
-struct IDirectMusicLoader : public IUnknown {
-  virtual long __stdcall GetObjectA(DMUS_OBJECTDESC *myDesc, const GUID &riid, void **ppv) = 0;
-  virtual long __stdcall SetSearchDirectory(const GUID &, wchar_t *, int) = 0;
-  virtual long __stdcall SetObject(DMUS_OBJECTDESC *) = 0;
-  virtual long __stdcall ScanDirectory(const GUID &, wchar_t *, wchar_t *) = 0;
-  virtual long __stdcall CacheObject(IDirectMusicObject *) = 0;
-  virtual long __stdcall ReleaseObject(IDirectMusicObject *) = 0;
-  virtual long __stdcall ClearCache(const GUID &) = 0;
-  virtual long __stdcall EnableCache(const GUID &, int) = 0;
-  virtual long __stdcall EnumObject(const GUID &, unsigned long, DMUS_OBJECTDESC *) = 0;
-};
-
-struct IDirectMusicGetLoader : public IUnknown {
-  virtual long __stdcall GetLoader(IDirectMusicLoader **ppLoader) = 0;
 };
 
 class MY_DMUS_OBJECTDESC : public DMUS_OBJECTDESC {
@@ -63,8 +27,8 @@ class CMyLoader : public IDirectMusicLoader {
   virtual unsigned long __stdcall AddRef();
   virtual unsigned long __stdcall Release();
   virtual long __stdcall          GetObjectA(DMUS_OBJECTDESC *myDesc, const GUID &riid, void **ppv);
-  virtual long __stdcall          SetSearchDirectory(const GUID &, wchar_t *, int);
   virtual long __stdcall          SetObject(DMUS_OBJECTDESC *);
+  virtual long __stdcall          SetSearchDirectory(const GUID &, wchar_t *, int);
   virtual long __stdcall          ScanDirectory(const GUID &, wchar_t *, wchar_t *);
   virtual long __stdcall          CacheObject(IDirectMusicObject *__formal);
   virtual long __stdcall          ReleaseObject(IDirectMusicObject *__formal);
@@ -111,73 +75,12 @@ class CMyIStream : public IStream, public IDirectMusicGetLoader {
   ASYNCLOADER *m_loader;
 };
 
-static const GUID CLSID_DirectMusicSegment = {
-    0xd2ac2882,
-    0xb39b,
-    0x11d1,
-    {0x87, 0x04, 0x00, 0x60, 0x08, 0x93, 0xb1, 0xbd}
-};
-static const GUID CLSID_DirectMusicCollection = {
-    0x480ff4b0,
-    0x28b2,
-    0x11d1,
-    {0xbe, 0xf7, 0x00, 0xc0, 0x4f, 0xbf, 0x8f, 0xef}
-};
-static const GUID IID_IDirectMusicSegment8 = {
-    0xc6784488,
-    0x41a3,
-    0x418f,
-    {0xaa, 0x15, 0xb3, 0x50, 0x93, 0xba, 0x42, 0xd4}
-};
-static const GUID IID_IDirectMusicCollection = {
-    0xd2ac287c,
-    0xb39b,
-    0x11d1,
-    {0x87, 0x04, 0x00, 0x60, 0x08, 0x93, 0xb1, 0xbd}
-};
-static const GUID GUID_Download = {
-    0x1db1ae6b,
-    0xe92e,
-    0x11d1,
-    {0xa8, 0xc5, 0x00, 0xc0, 0x4f, 0xa3, 0x72, 0x6e}
-};
-static const GUID IID_IDirectMusicObject = {
-    0xd2ac28b5,
-    0xb39b,
-    0x11d1,
-    {0x87, 0x04, 0x00, 0x60, 0x08, 0x93, 0xb1, 0xbd}
-};
-static const GUID IID_IDirectMusicGetLoader = {
-    0x68a04844,
-    0xd13d,
-    0x11d1,
-    {0xaf, 0xa6, 0x00, 0xaa, 0x00, 0x24, 0xd8, 0xb6}
-};
-static const GUID IID_IDirectMusicLoader = {
-    0x2ffaaca2,
-    0x5dca,
-    0x11d2,
-    {0xaf, 0xa6, 0x00, 0xaa, 0x00, 0x24, 0xd8, 0xb6}
-};
-static const GUID CLSID_DirectMusicSegmentState = {
-    0xd2ac288a,
-    0xb39b,
-    0x11d1,
-    {0x87, 0x04, 0x00, 0x60, 0x08, 0x93, 0xb1, 0xbd}
-};
-static const GUID CLSID_DirectMusicWaveTrack = {
-    0x8a667154,
-    0xf9cb,
-    0x11d2,
-    {0xad, 0x8a, 0x00, 0x60, 0xb0, 0x57, 0x5a, 0xbc}
-};
-
 static CMyLoader s_loader;
 
-static void        *s_dmusicPerformance;
-static void        *s_dmusicSegment;
-static void        *s_dmusicPath;
-static void        *s_dmusicCollection;
+static IDirectMusicPerformance8 *s_dmusicPerformance;
+static IDirectMusicSegment8     *s_dmusicSegment;
+static IDirectMusicAudioPath    *s_dmusicPath;
+static IDirectMusicCollection   *s_dmusicCollection;
 static unsigned int s_initialized;
 static ASYNCLOADER  s_MID;
 static ASYNCLOADER  s_DLS;
@@ -253,19 +156,14 @@ long __stdcall CMyLoader::EnumObject(const GUID &, unsigned long, DMUS_OBJECTDES
 
 static void __fastcall MIDI_CleanupSegment() {
   if (s_dmusicPerformance && s_dmusicSegment) {
-    typedef long(__stdcall * UnloadFn)(void *, void *);
-    UnloadFn unload = *reinterpret_cast<UnloadFn *>(*reinterpret_cast<unsigned char **>(s_dmusicSegment) + 120);
-    unload(s_dmusicSegment, s_dmusicPerformance);
+    s_dmusicSegment->Unload(s_dmusicPerformance);
 
-    typedef unsigned long(__stdcall * ReleaseFn)(void *);
     if (s_dmusicCollection) {
-      ReleaseFn releaseCollection = *reinterpret_cast<ReleaseFn *>(*reinterpret_cast<unsigned char **>(s_dmusicCollection) + 8);
-      releaseCollection(s_dmusicCollection);
+      s_dmusicCollection->Release();
       s_dmusicCollection = 0;
     }
 
-    ReleaseFn releaseSegment = *reinterpret_cast<ReleaseFn *>(*reinterpret_cast<unsigned char **>(s_dmusicSegment) + 8);
-    releaseSegment(s_dmusicSegment);
+    s_dmusicSegment->Release();
     s_dmusicSegment = 0;
   }
 }
@@ -281,9 +179,9 @@ void __fastcall PostLoadCallback(void *userArg) {
   objDesc.guidClass = CLSID_DirectMusicSegment;
   objDesc.asyncLoader = &s_MID;
   objDesc.llMemLength = s_MID.buffer.Count();
-  objDesc.pbMemData = reinterpret_cast<unsigned int *>(s_MID.buffer.Ptr());
+  objDesc.pbMemData = reinterpret_cast<unsigned char *>(s_MID.buffer.Ptr());
   objDesc.pStream = 0;
-  if (s_loader.GetObjectA(&objDesc, IID_IDirectMusicSegment8, &s_dmusicSegment)) {
+  if (s_loader.GetObjectA(&objDesc, IID_IDirectMusicSegment8, reinterpret_cast<void **>(&s_dmusicSegment))) {
     MIDI_CleanupSegment();
     return;
   }
@@ -291,37 +189,29 @@ void __fastcall PostLoadCallback(void *userArg) {
   objDesc.guidClass = CLSID_DirectMusicCollection;
   objDesc.asyncLoader = &s_DLS;
   objDesc.llMemLength = s_DLS.buffer.Count();
-  objDesc.pbMemData = reinterpret_cast<unsigned int *>(s_DLS.buffer.Ptr());
+  objDesc.pbMemData = reinterpret_cast<unsigned char *>(s_DLS.buffer.Ptr());
   objDesc.pStream = 0;
-  if (s_loader.GetObjectA(&objDesc, IID_IDirectMusicCollection, &s_dmusicCollection)) {
+  if (s_loader.GetObjectA(&objDesc, IID_IDirectMusicCollection, reinterpret_cast<void **>(&s_dmusicCollection))) {
     MIDI_CleanupSegment();
     return;
   }
 
-  typedef long(__stdcall * SetParamFn)(void *, const GUID *, long, unsigned long, unsigned long, void *);
-  SetParamFn setParam = *reinterpret_cast<SetParamFn *>(*reinterpret_cast<unsigned char **>(s_dmusicSegment) + 76);
-  if (setParam(s_dmusicSegment, &GUID_Download, -1, 0x80000000, 0, s_dmusicCollection)) {
+  if (s_dmusicSegment->SetParam(GUID_ConnectToDLSCollection, -1, 0x80000000, 0, s_dmusicCollection)) {
     MIDI_CleanupSegment();
     return;
   }
 
-  typedef long(__stdcall * GetAudioPathConfigFn)(void *, void **);
-  GetAudioPathConfigFn getAudioPathConfig = *reinterpret_cast<GetAudioPathConfigFn *>(*reinterpret_cast<unsigned char **>(s_dmusicSegment) + 116);
-  if (getAudioPathConfig(s_dmusicSegment, &s_dmusicPath)) {
+  if (s_dmusicSegment->GetAudioPathConfig(reinterpret_cast<IUnknown **>(&s_dmusicPath))) {
     MIDI_CleanupSegment();
     return;
   }
 
-  typedef long(__stdcall * SetRepeatsFn)(void *, long);
-  SetRepeatsFn setRepeats = *reinterpret_cast<SetRepeatsFn *>(*reinterpret_cast<unsigned char **>(s_dmusicSegment) + 24);
-  if (setRepeats(s_dmusicSegment, -1)) {
+  if (s_dmusicSegment->SetRepeats(-1)) {
     MIDI_CleanupSegment();
     return;
   }
 
-  typedef long(__stdcall * PlaySegmentExFn)(void *, void *, void *, void *, unsigned long, __int64, void *, void *, void *);
-  PlaySegmentExFn playSegment = *reinterpret_cast<PlaySegmentExFn *>(*reinterpret_cast<unsigned char **>(s_dmusicPerformance) + 180);
-  if (playSegment(s_dmusicPerformance, s_dmusicSegment, 0, 0, 0, 0, 0, 0, s_dmusicPath)) {
+  if (s_dmusicPerformance->PlaySegmentEx(s_dmusicSegment, 0, 0, 0, 0, 0, 0, s_dmusicPath)) {
     MIDI_CleanupSegment();
   }
 }
@@ -360,9 +250,7 @@ void __fastcall Sound::MIDI_Play(const char *midiFilename, const char *dlsFilena
 
 void __fastcall Sound::MIDI_Stop() {
   if (s_initialized && s_dmusicSegment) {
-    typedef long(__stdcall * StopFn)(void *, void *, void *, unsigned long, unsigned long);
-    StopFn stop = *reinterpret_cast<StopFn *>(*reinterpret_cast<unsigned char **>(s_dmusicPerformance) + 184);
-    stop(s_dmusicPerformance, s_dmusicSegment, 0, 0, 0);
+    s_dmusicPerformance->StopEx(s_dmusicSegment, 0, 0);
     MIDI_CleanupSegment();
   }
 }
@@ -370,9 +258,7 @@ void __fastcall Sound::MIDI_Stop() {
 void __fastcall Sound::MIDI_SetVolume(float volume) {
   if (s_initialized) {
     ASSERT(volume >= 0.0f && volume <= 1.0f);
-    typedef long(__stdcall * SetVolumeFn)(void *, long, unsigned long);
-    SetVolumeFn setVolume = *reinterpret_cast<SetVolumeFn *>(*reinterpret_cast<unsigned char **>(s_dmusicPath) + 20);
-    setVolume(s_dmusicPath, -9600 - static_cast<long>(volume * -9600.0f), 0);
+    s_dmusicPath->SetVolume(-9600 - static_cast<long>(volume * -9600.0f), 0);
   }
 }
 
@@ -389,7 +275,7 @@ long __stdcall CMyLoader::GetObjectA(DMUS_OBJECTDESC *myDesc, const GUID &riid, 
   DMUS_OBJECTDESC DESC;
   wchar_t         wzFileName[260];
   char            name[260];
-  IUnknown       *pObject = 0;
+  IDirectMusicObject *pObject = 0;
   IPersistStream *pPersistStream = 0;
   const GUID     *pGUID = &myDesc->guidClass;
 
@@ -420,14 +306,12 @@ long __stdcall CMyLoader::GetObjectA(DMUS_OBJECTDESC *myDesc, const GUID &riid, 
       }
 
       if (status >= 0) {
-        if (!memcmp(pGUID, &CLSID_DirectMusicSegmentState, sizeof(*pGUID)) || !memcmp(pGUID, &CLSID_DirectMusicWaveTrack, sizeof(*pGUID)) ||
+        if (!memcmp(pGUID, &CLSID_DirectMusicStyle, sizeof(*pGUID)) || !memcmp(pGUID, &CLSID_DirectSoundWave, sizeof(*pGUID)) ||
             !memcmp(pGUID, &CLSID_DirectMusicCollection, sizeof(*pGUID)))
         {
           memset(&DESC, 0, sizeof(DESC));
           DESC.dwSize = sizeof(DESC);
-          typedef long(__stdcall * GetDescriptorFn)(void *, DMUS_OBJECTDESC *);
-          GetDescriptorFn getDescriptor = *reinterpret_cast<GetDescriptorFn *>(*reinterpret_cast<unsigned char **>(pObject) + 12);
-          getDescriptor(pObject, &DESC);
+          pObject->GetDescriptor(&DESC);
         }
         status = pObject->QueryInterface(riid, ppv);
       }
